ad80d894388b261656b24cd3c2f89551331aa8ee
Janssen125 <janssenaddisonchen@gmail.com>
2022-10-20 09:17:13 +0700

    sabar ya yog

--- frontend/dashboard/index.php
+++ frontend/dashboard/index.php
@@ -221,46 +221,6 @@ if ($_SESSION['hak_akses'] == "Murid") {
         <div class="content-wrapper">
           <!-- Content -->
 
-          <div id="carouselExample" class="carousel slide carousel-fade container-xxl flex-grow-1 container-p-y" data-bs-ride="carousel">
-            <ol class="carousel-indicators">
-              <li data-bs-target="#carouselExample" data-bs-slide-to="0" class="active"></li>
-              <li data-bs-target="#carouselExample" data-bs-slide-to="1"></li>
-              <li data-bs-target="#carouselExample" data-bs-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img class="d-block w-100" src="../assets/assets/img/elements/IMG_2666.JPG" alt="First slide" />
-                <div class="carousel-caption d-none d-md-block">
-                  <h3>Eskul Basket</h3>
-                  <p>olahraga bola berkelompok yang terdiri atas dua tim beranggotakan masing-masing lima orang yang saling bertanding mencetak poin dengan memasukkan bola ke dalam keranjang lawan.</p>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="../assets/assets/img/elements/IMG_3190.JPG" alt="Second slide" />
-                <div class="carousel-caption d-none d-md-block">
-                  <h3>Eskul Pramuka</h3>
-                  <p>sistem pendidikan kepanduan yang disesuaikan dengan keadaan, kepentingan dan perkembangan masyarakat dan bangsa Indonesia.</p>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <img class="d-block w-100" src="../assets/assets/img/elements/IMG_6395.JPG" alt="Third slide" />
-                <div class="carousel-caption d-none d-md-block">
-                  <h3>Eskul Padus</h3>
-                  <p>istilah untuk menunjukkan ensemble musik yang terdiri dari musik dan penyanyi-penyanyi yang menyanyikan lagu dalam harmoni menggunakan jenis suara yang berbeda-beda.</p>
-                </div>
-              </div>
-            </div>
-            <a class="carousel-control-prev" href="#carouselExample" role="button" data-bs-slide="prev">
-              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Previous</span>
-            </a>
-            <a class="carousel-control-next" href="#carouselExample" role="button" data-bs-slide="next">
-              <span class="carousel-control-next-icon" aria-hidden="true"></span>
-              <span class="visually-hidden">Next</span>
-            </a>
-          </div>
-        </div>
-
             <div id="carouselExample" class="carousel slide carousel-fade container-xxl flex-grow-1 container-p-y" data-bs-ride="carousel">
                     <ol class="carousel-indicators">
                       <li data-bs-target="#carouselExample" data-bs-slide-to="0" class="active"></li>
