76968f96a93663320bc85900eaa41c982d237cf6
Felicia Natasha Pratama <feliciapratama26@gmail.com>
2022-10-20 07:26:17 +0700

    register

--- frontend/index.php
+++ frontend/index.php
@@ -97,7 +97,7 @@ session_start();
                             <?php
                                     if(!isset($_SESSION['username'])){
                                         echo "<li><a class='main-btn btn-one' href='login/'>Login</a></li>
-                                        <li><a class='main-btn btn-two' href='#'>Register</i></a></li>";
+                                        <li><a class='main-btn btn-two' href='register/'>Register</i></a></li>";
                                     }
                                     else{
                                         echo "<li><a class='main-btn btn-one' href='#'>Ayo Lihat Daftar Ekskul</a></li>";
@@ -281,7 +281,7 @@ session_start();
                             <?php
                                     if(!isset($_SESSION['username'])){
                                         echo "<li><a href='login/'>Login</a></li>
-                                        <li><a href='#'>Register</a></li>";
+                                        <li><a href='register/'>Register</a></li>";
                                     }
                                     else{
                                         echo "<li><a href='../backend/logout.php'>Logout</a></li>";
