cbf1f485cb736f4a624bfe8aff36227679dc627a
Janssen125 <janssenaddisonchen@gmail.com>
2022-11-01 13:53:27 +0700

    horeeeee

--- frontend/dashboard/luar.php
+++ frontend/dashboard/luar.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
 if ($_SESSION['hak_akses'] == "Murid") {
-    echo "<script>location.href=('../')</script>";
+    echo "<script>location.href=('../../')</script>";
 }
 ?>
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default" data-assets-path="../assets/" data-template="vertical-menu-template-free">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="light-style layout-menu-fixed" dir="ltr" data-theme="theme-default" data-assets-path="../../assets/" data-template="vertical-menu-template-free">
 
 <head>
     <meta charset="utf-8" />
@@ -16,7 +16,7 @@ if ($_SESSION['hak_akses'] == "Murid") {
     <meta name="description" content="" />
 
     <!-- Favicon -->
-    <link rel="icon" type="image/x-icon" href="../assets/images/logo-cktc.png" />
+    <link rel="icon" type="image/x-icon" href="../../assets/images/logo-cktc.png" />
 
     <!-- Fonts -->
     <link rel="preconnect" href="https://fonts.googleapis.com" />
@@ -24,26 +24,26 @@ if ($_SESSION['hak_akses'] == "Murid") {
     <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
 
     <!-- Icons. Uncomment required icon fonts -->
-    <link rel="stylesheet" href="../assets/assets/vendor/fonts/boxicons.css" />
+    <link rel="stylesheet" href="../../assets/assets/vendor/fonts/boxicons.css" />
 
     <!-- Core CSS -->
-    <link rel="stylesheet" href="../assets/assets/vendor/css/core.css" class="template-customizer-core-css" />
-    <link rel="stylesheet" href="../assets/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
-    <link rel="stylesheet" href="../assets/assets/css/demo.css" />
+    <link rel="stylesheet" href="../../assets/assets/vendor/css/core.css" class="template-customizer-core-css" />
+    <link rel="stylesheet" href="../../assets/assets/vendor/css/theme-default.css" class="template-customizer-theme-css" />
+    <link rel="stylesheet" href="../../assets/assets/css/demo.css" />
 
     <!-- Vendors CSS -->
-    <link rel="stylesheet" href="../assets/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
+    <link rel="stylesheet" href="../../assets/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css" />
 
-    <link rel="stylesheet" href="../assets/assets/vendor/libs/apex-charts/apex-charts.css" />
+    <link rel="stylesheet" href="../../assets/assets/vendor/libs/apex-charts/apex-charts.css" />
 
     <!-- Page CSS -->
 
     <!-- Helpers -->
-    <script src="../assets/assets/vendor/js/helpers.js"></script>
+    <script src="../../assets/assets/vendor/js/helpers.js"></script>
 
     <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
     <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
-    <script src="../assets/assets/js/config.js"></script>
+    <script src="../../assets/assets/js/config.js"></script>
 </head>
 
 <body>
@@ -54,9 +54,9 @@ if ($_SESSION['hak_akses'] == "Murid") {
 
             <aside id="layout-menu" class="layout-menu menu-vertical menu bg-menu-theme">
                 <div class="app-brand demo">
-                    <a href="#" class="app-brand-link">
+                    <a href="../" class="app-brand-link">
                         <span class="app-brand-logo demo">
-                            <img width="58" src="../assets/images/logo-cktc.png">
+                            <img width="58" src="../../assets/images/logo-cktc.png">
                             <defs>
                                 <path d="M13.7918663,0.358365126 L3.39788168,7.44174259 C0.566865006,9.69408886 -0.379795268,12.4788597 0.557900856,15.7960551 C0.68998853,16.2305145 1.09562888,17.7872135 3.12357076,19.2293357 C3.8146334,19.7207684 5.32369333,20.3834223 7.65075054,21.2172976 L7.59773219,21.2525164 L2.63468769,24.5493413 C0.445452254,26.3002124 0.0884951797,28.5083815 1.56381646,31.1738486 C2.83770406,32.8170431 5.20850219,33.2640127 7.09180128,32.5391577 C8.347334,32.0559211 11.4559176,30.0011079 16.4175519,26.3747182 C18.0338572,24.4997857 18.6973423,22.4544883 18.4080071,20.2388261 C17.963753,17.5346866 16.1776345,15.5799961 13.0496516,14.3747546 L10.9194936,13.4715819 L18.6192054,7.984237 L13.7918663,0.358365126 Z" id="path-1"></path>
                                 <path d="M5.47320593,6.00457225 C4.05321814,8.216144 4.36334763,10.0722806 6.40359441,11.5729822 C8.61520715,12.571656 10.0999176,13.2171421 10.8577257,13.5094407 L15.5088241,14.433041 L18.6192054,7.984237 C15.5364148,3.11535317 13.9273018,0.573395879 13.7918663,0.358365126 C13.5790555,0.511491653 10.8061687,2.3935607 5.47320593,6.00457225 Z" id="path-3"></path>
@@ -102,7 +102,7 @@ if ($_SESSION['hak_akses'] == "Murid") {
                 <ul class="menu-inner py-1">
                     <!-- Dashboard -->
                     <li class="menu-item active">
-                        <a href="/" class="menu-link">
+                        <a href="../" class="menu-link">
                             <i class="menu-icon tf-icons bx bx-home-circle"></i>
                             <div data-i18n="Analytics">Dashboard</div>
                         </a>
@@ -184,7 +184,7 @@ if ($_SESSION['hak_akses'] == "Murid") {
                             <li class="nav-item navbar-dropdown dropdown-user dropdown">
                                 <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);" data-bs-toggle="dropdown">
                                     <div class="avatar avatar-online">
-                                        <img src="../assets/assets/img/avatars/avatar.png" alt class="w-px-40 h-auto rounded-circle" />
+                                        <img src="../../assets/assets/img/avatars/avatar.png" alt class="w-px-40 h-auto rounded-circle" />
                                     </div>
                                 </a>
                                 <ul class="dropdown-menu dropdown-menu-end">
@@ -193,7 +193,7 @@ if ($_SESSION['hak_akses'] == "Murid") {
                                             <div class="d-flex">
                                                 <div class="flex-shrink-0 me-3">
                                                     <div class="avatar avatar-online">
-                                                        <img src="../assets/assets/img/avatars/avatar.png" alt class="w-px-40 h-auto rounded-circle" />
+                                                        <img src="../../assets/assets/img/avatars/avatar.png" alt class="w-px-40 h-auto rounded-circle" />
                                                     </div>
                                                 </div>
                                                 <span class="fw-semibold d-block">
@@ -214,7 +214,7 @@ if ($_SESSION['hak_akses'] == "Murid") {
                                         <div class="dropdown-divider"></div>
                                     </li>
                                     <li>
-                                        <a class="dropdown-item" href="../../backend/logout.php">
+                                        <a class="dropdown-item" href="../../../../backend/logout.php">
                                             <i class="bx bx-power-off me-2"></i>
 
                                             <span class="align-middle">Log Out</span>
---
+++ frontend/dashboard/user/index.php
@@ -0,0 +1,7 @@
+<?php
+include '../luar.php';
+?>
+
+<?php
+include '../footer.php';
+?>
