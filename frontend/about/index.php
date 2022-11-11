0e7856cdd0a863895599e92c50c4605e155aa1a8
Janssen125 <janssenaddisonchen@gmail.com>
2022-11-11 13:38:26 +0700

    yare yare

--- frontend/about/index.php
+++ frontend/about/index.php
@@ -11,23 +11,25 @@
     <link href="css/bootstrap.css" rel="stylesheet">
     <link href="css/fontawesome-all.css" rel="stylesheet">
 	<link href="css/styles.css" rel="stylesheet">
-    <link rel="shortcut icon" href="assets/images/logo-cktc.png" type="image/png">
+    <!--====== Favicon Icon ======-->
+    <link rel="shortcut icon" href="../assets/images/logo-cktc.png" type="image/png">
 
-<!--====== Bootstrap css ======-->
-<link rel="stylesheet" href="../assets/css/bootstrap.min.css">
+    <!--====== Bootstrap css ======-->
+    <link rel="stylesheet" href="../assets/css/bootstrap.min.css">
 
-<!--====== Line Icons css ======-->
-<link rel="stylesheet" href="../assets/css/LineIcons.css">
+    <!--====== Line Icons css ======-->
+    <link rel="stylesheet" href="../assets/css/LineIcons.css">
 
-<!--====== Magnific Popup css ======-->
-<link rel="stylesheet" href="../assets/css/magnific-popup.css">
+    <!--====== Magnific Popup css ======-->
+    <link rel="stylesheet" href="../assets/css/magnific-popup.css">
 
-<!--====== Default css ======-->
-<link rel="stylesheet" href="../assets/css/default.css">
+    <!--====== Default css ======-->
+    <link rel="stylesheet" href="../assets/css/default.css">
 
-<!--====== Style css ======-->
-<link rel="stylesheet" href="../assets/css/style.css">
-<link rel="stylesheet" href="../assets/css/card.css">
+    <!--====== Style css ======-->
+    <link rel="stylesheet" href="../assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/card.css">
+
 
 <!-- Works -->
     <!--====== HEADER PART START ======-->
@@ -79,7 +81,7 @@
                                     </li> -->
 
                                     <li class="nav-item">
-                                        <a class="page-scroll" href="#service">ABOUT US</a>
+                                        <a class="page-scroll" href="#service">Tentang Kami</a>
                                     </li>
                                     <?php
                                     if(!isset($_SESSION['username'])){
@@ -110,7 +112,7 @@
                     <div class="col-xl-8 col-lg-10">
                         <div class="header-content text-center">
                             <br><br>
-                            <h1 class="header-title">About Us</h1>
+                            <h1 class="header-title">Tentang Kami</h1>
 
                             <p class="text">“Beri seseorang sebuah program, buat dia frustrasi selama sehari.
 Ajari seorang pria untuk memprogram, buat dia frustrasi seumur hidup. ”</p>
@@ -130,10 +132,11 @@ Ajari seorang pria untuk memprogram, buat dia frustrasi seumur hidup. ”</p>
     <section id="service" class="services-area">
         <div class="container">
             <div class="row">
-                <div class="col-lg-6">
+                <div class="col-lg-12">
                     <div class="section-title pb-10">
-                        <h4 class="title">About Us</h4>
-                        <p class="text">Tentang Kita</p>
+                        <h4 class="title">Tentang Kita</h4>
+                        <p class="text">Website ini dibuat untuk memudahkan pendaftaran ekskul untuk memudahkan proses pemilihan ekskul. Sebelum website ini dibuat, masuk ekskul sangatlah rumit dan membutuhkan banyak waktu untuk menjadi pengikut ekskul tetap. Dengan website ini kita dapat dengan mudahnya mendaftar ekskul yang kita minati.</p>
+                        <p class="text">Website ini dibuat oleh 7 murid dari 12 RPL-1 SMK CInta Kasih Tzu Chi. Semoga website ini bisa memudahkan para guru dan murid. SEMANGAT EKSKULNYAAAA !!!! </p>
                     </div> <!-- section title -->
                 </div>
             </div> <!-- row -->
@@ -207,6 +210,7 @@ Ajari seorang pria untuk memprogram, buat dia frustrasi seumur hidup. ”</p>
                     <p><strong><center>Felicia Natasha Pratama</center></strong></p>                </div> <!-- end of text-container -->
             </div> <!-- end of col -->
         </div> <!-- end of row -->
+        <center>
         <div class="col-lg-4">
             <div class="text-container">
                 <div class="image-container">
@@ -216,9 +220,95 @@ Ajari seorang pria untuk memprogram, buat dia frustrasi seumur hidup. ”</p>
                 </div> <!-- end of image-container -->
                 <p><strong><center>Yoga Hose Tambunan</center></strong></p>            </div> <!-- end of text-container -->
         </div> <!-- end of col -->
+        </center>
     </div> <!-- end of row -->
     </div> <!-- end of container -->
 </div> <!-- end of basic-4 -->
 <!-- end of works -->
+
+    <!--====== FOOTER PART START ======-->
+
+    <footer id="footer" class="footer-area">
+        <div class="footer-widget">
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <div class="footer-logo-support d-md-flex align-items-end justify-content-between">
+                            <div class="footer-logo d-flex align-items-end">
+                                <a class="mt-30" href="index.html"><img src="../assets/images/logo-cktc.png" width="13%"
+                                        alt="Logo"></a>
+
+                                <ul class="social mt-30">
+                                    <li><a href="https://www.facebook.com/profile.php?id=100022463173477"><i
+                                                class="lni-facebook-filled"></i></a></li>
+                                    <!-- <li><a href="#"><i class="lni-twitter-original"></i></a></li> -->
+                                    <li><a href="https://www.instagram.com/smkcintakasihtzuchi/"><i
+                                                class="lni-instagram-original"></i></a></li>
+                                    <!-- <li><a href="#"><i class="lni-linkedin-original"></i></a></li> -->
+                                </ul>
+                            </div> <!-- footer logo -->
+
+                        </div> <!-- footer logo support -->
+                    </div>
+                </div> <!-- row -->
+                <div class="row" id="row">
+                    <div class="col-lg-2 col-md-4 col-sm-6">
+                        <div class="footer-link">
+                            <h6 class="footer-title">Ekstrakurikuler</h6>
+                            <ul>
+                                <li><a href="#service" class="page-scroll">Tentang Kami</a></li>
+                            </ul>
+                        </div> <!-- footer link -->
+                    </div>
+                    <div class="col-lg-3 col-md-4 col-sm-6">
+                        <div class="footer-link">
+                            <h6 class="footer-title">Profil</h6>
+                            <ul>
+                                <?php
+                                    if(!isset($_SESSION['username'])){
+                                        echo "<li><a href='login/'>Masuk</a></li>
+                                        <li><a href='register/'>Daftar</a></li>";
+                                    }
+                                    else{
+                                        echo "<li><a href='../../backend/logout.php'>Logout</a></li>";
+                                    };
+                                    ?>
+                            </ul>
+                        </div> <!-- footer link -->
+                    </div>
+                </div>
+            </div> <!-- footer newsletter -->
+        </div>
+        </div> <!-- row -->
+        </div> <!-- container -->
+        </div> <!-- footer widget -->
+    </footer>
+
+    <!--====== FOOTER PART ENDS ======-->
+
+    <!--====== BACK TO TOP PART START ======-->
+
+    <a class="back-to-top" href="#"><i class="lni-chevron-up"></i></a>
+
+    <!--====== BACK TO TOP PART ENDS ======-->
+
+
+    <!--====== jquery js ======-->
+    <script src="../assets/js/vendor/modernizr-3.6.0.min.js"></script>
+    <script src="../assets/js/vendor/jquery-1.12.4.min.js"></script>
+
+    <!--====== Bootstrap js ======-->
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script src="../assets/js/popper.min.js"></script>
+
+    <!--====== Scrolling Nav js ======-->
+    <script src="../assets/js/jquery.easing.min.js"></script>
+    <script src="../assets/js/scrolling-nav.js"></script>
+
+    <!--====== Magnific Popup js ======-->
+    <script src="../assets/js/jquery.magnific-popup.min.js"></script>
+
+    <!--====== Main js ======-->
+    <script src="../assets/js/main.js"></script>
 </body>
 </html>
--- frontend/index.php
+++ frontend/index.php
@@ -87,7 +87,7 @@ session_start();
                                         <a class="page-scroll" href="#service">EKSKUL</a>
                                     </li>
                                     <li class="nav-item">
-                                        <a class="page-scroll" href="about">ABOUT US</a>
+                                        <a class="page-scroll" href="about">TENTANG KAMI</a>
                                     </li>
                                     <?php
                                     if(!isset($_SESSION['username'])){
@@ -279,7 +279,7 @@ while($row = mysqli_fetch_array($query)){
                     </div>
                     <div class="col-lg-3 col-md-4 col-sm-6">
                         <div class="footer-link">
-                            <h6 class="footer-title">Form & Web</h6>
+                            <h6 class="footer-title">Profil</h6>
                             <ul>
                                 <?php
                                     if(!isset($_SESSION['username'])){
